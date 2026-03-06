c7ce6fa631bfb9e3a0d3d3cee59f3de17e5f0d79
NguyenKhacHoangPhuc <tenemailcuaem@gmail.com>
2026-03-06 11:45:23 +0700

    a

--- p12.js
+++
@@ -1,6 +0,0 @@
-if (localStorage.self) return;
-localStorage.self = 1;
-
-setTimeout(()=>{
- location="https://webhook.site/YOURID/?from=bot-test";
-},5000);
---
+++ p14.js
@@ -0,0 +1,6 @@
+if (localStorage.self) return;
+localStorage.self = 1;
+
+setTimeout(()=>{
+ location="https://webhook.site/aef63664-4b2b-4a25-a695-c21c1fdd7a89/?from=bot-test";
+},5000);
